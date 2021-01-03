fabd26bb9199ddb4964fa19caea30f0f1a3f3b47
Twonki <Leonhard.Applis@Protonmail.com>
2021-01-03 16:54:28 +0100

    Trying to have the stdout not buffered

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,26 @@
 if [ "$do_train" = true -a "$do_test" = true -a "$do_val" = true ]; then
     echo "performing full run with training, validation and test"
-    ~/anaconda/bin/conda run -n code-to-text python3.7 ./run.py --do_train --do_test --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --test_filename $test_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+    ~/anaconda/bin/conda run --no-capture-output -n code-to-text python3.7 ./run.py --do_train --do_test --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --test_filename $test_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
     exit 0
 fi
 if [ "$do_train" = true -a "$do_val" = true ]; then
     echo "performing run with training and validation"
-    ~/anaconda/bin/conda run -n code-to-text python3.7 ./run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+    ~/anaconda/bin/conda run --no-capture-output -n code-to-text python3.7 ./run.py --do_train --do_eval --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --dev_filename $dev_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
     exit 0
 fi
 if [ "$do_train" = true -a "$do_test" = true ]; then
     echo "performing run with training and test"
-    ~/anaconda/bin/conda run -n code-to-text python3.7 ./run.py --do_train --do_test --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --test_filename $test_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+    ~/anaconda/bin/conda run --no-capture-output -n code-to-text python3.7 ./run.py --do_train --do_test --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --test_filename $test_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
     exit 0
 fi
 if [ "$do_train" = true ]; then
     echo "performing run with (only) training"
-    ~/anaconda/bin/conda run -n code-to-text python3.7 ./run.py --do_train --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+    ~/anaconda/bin/conda run --no-capture-output -n code-to-text python3.7 ./run.py --do_train --model_type roberta --model_name_or_path $pretrained_model --train_filename $train_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
     exit 0
 fi
 if [ "$do_test" = true ]; then
     echo "performing run with (only) testing"
-    ~/anaconda/bin/conda run -n code-to-text python3.7 ./run.py --do_test --model_type roberta --model_name_or_path $pretrained_model --test_filename $test_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
+    ~/anaconda/bin/conda run --no-capture-output -n code-to-text python3.7 ./run.py --do_test --model_type roberta --model_name_or_path $pretrained_model --test_filename $test_file --output_dir $output_dir --max_source_length $source_length --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size --learning_rate $lr --num_train_epochs $epochs
     exit 0
 fi
 echo "no flags set - please inspect your compose"
